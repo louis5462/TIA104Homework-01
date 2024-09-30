34a746d930200cce22a4e9cde680627b7e4efb44
Louis Chang <zu5462@gmail.com>
2024-09-30 16:05:14 +0800

    1st verison, add HW1,HW2

---
+++ src/homework_01/HW1.java
@@ -0,0 +1,53 @@
+package homework_01;
+
+public class HW1 {
+	public static void main(String[] args) {
+		calcSumAndProudct(12,6);
+		calcEggs(200);
+		calcTimeIntoDate(256559);
+		circleAreaAndCircumference(5);
+		calcRate(1_500_000,0.02,10);
+		typeChange ();
+	}
+//		請設計一隻Java程式,計算12,6這兩個數值的和與積
+	public static void calcSumAndProudct(int i,int j) {
+		 System.out.println(i+"跟"+j+"的和為:"+(i+j)+"，積為:"+i*j);
+	}
+//		• 請設計一隻Java程式,計算200顆蛋共是幾打幾顆? (一打為12顆)
+	public static void calcEggs(int i) {
+		System.out.println(i+"顆的雞蛋是，"+i/12+"打又"+i%12+"顆");
+}
+//		• 請由程式算出256559秒為多少天、多少小時、多少分與多少秒
+	public static void calcTimeIntoDate(int i) {
+		int days = i/(60*60*24) ; 
+		int hours = i/(60*60) - days*24 ; 
+		int minutes = i/60 - hours*60 - days*(24*60) ;
+		int sec = i - minutes*60 - hours*(60*60) - days*(24*60*60) ;
+		System.out.println(i+"秒是"+days+"天又"+hours+"小時又"+minutes+"分又"+sec+"秒。");
+	}
+//		• 請定義一個常數為3.1415(圓周率),並計算半徑為5的圓面積與圓周長
+	 public static void circleAreaAndCircumference(double i){
+		 double pi =3.1415;
+		 System.out.println("半徑為"+i+"的圓，其圓周長為"+String.format("%.4f",2*i*pi,4f)+"，面積為"+String.format("%.4f",pi*Math.pow(i, 2)));
+	}
+//		• 某人在銀行存入150萬,銀行利率為2%,如果每年利息都繼續存入銀行,請用程式計算10年後,本
+//		金加利息共有多少錢 (用複利計算,公式請自行google)
+	 public static int calcRate(int value ,double rate, int year){
+		 System.out.println(year+"年後，本金加利息為"+Math.round(value*(float)Math.pow((1+rate),year)));
+		 return Math.round(value*(float)Math.pow((1+rate),year));
+	 }
+//		• 請寫一隻程式,利用System.out.println()印出以下三個運算式結果:
+//		5 + 5
+//		5 + ‘5’
+//		5 + “5”
+//		並請用註解各別說明答案的產生原因
+	 public static void typeChange () {
+		 System.out.println(5 + 5);
+		 //結果為10，會直接印出兩個整數相加（int + int）
+		 System.out.println(5 + '5'); 
+		 //結果為58, '5'為char型別，跟int型別相加後，晉升為53（unicode對應，5十進位值為53），故相加後為58
+		 System.out.println(5 + "5");
+		 //結果為"55"（String型別），數字與字串相加時，會轉為字串型別
+	 }
+	 	
+}
---
+++ src/homework_01/HW2.java
@@ -0,0 +1,168 @@
+package homework_01;
+
+public class HW2{
+	public static void main(String[] args) {
+		calcEvenNumSum(1,1000);
+		calcEvenNumProduct(1,10);
+		calcEvenNumProductW(1,10);
+		calcSquareList();
+		lotterySelect(1,49);
+		drawNumTri();
+		drawABCTri('A','F');
+		testNineNine ();
+	}
+//		• 請設計一隻Java程式,計算1~1000的偶數和 (2+4+6+8+...+1000)
+	public static void calcEvenNumSum(int start, int end) {
+		if (start >= end) {
+			System.out.println("起始值小於等於結束值，請重新輸入");
+		}else {
+			int sum = 0 ;
+			if ( start%2 !=0 ) start++;
+			if ( end%2 != 0 ) end--;
+			for ( ; start <= end ; start+=2) {
+				sum += start;
+			}
+			System.out.println(sum);
+		}
+	}
+//		• 請設計一隻Java程式,計算1~10的連乘積 (1*2*3*...*10) (用for迴圈)
+	public static void calcEvenNumProduct(int start, int end) {
+		if (start >= end) {
+			System.out.println("起始值小於等於結束值，請重新輸入");
+		}else {
+			int product = 1 ; 
+			for ( ; start <= end ; start++) product*=start ;
+			System.out.println(product);
+		}
+	}
+//		• 請設計一隻Java程式,計算1~10的連乘積 (1*2*3*...*10) (用while迴圈)
+	public static void calcEvenNumProductW(int start, int end) {
+		if (start >= end) {
+			System.out.println("起始值小於等於結束值，請重新輸入");
+		}else {
+			int product = 1 ; 
+			while(start <= end) {
+				product*=start ;
+				start++;
+			}
+			System.out.println(product);	
+		}
+	}
+//		• 請設計一隻Java程式,輸出結果為以下:
+//		1 4 9 16 25 36 49 64 81 100
+	public static void calcSquareList() {
+		String list = "" ;
+		for (int i = 1; i <=10 ; i++ ) list += String.format("%d ",(int)Math.pow(i, 2));
+		System.out.println(list);
+	}
+//		• 阿文很熱衷大樂透 (1 ~ 49),但他不喜歡有4的數字,不論是個位數或是十位數。請設計一隻程式,
+//		輸出結果為阿文可以選擇的數字有哪些?總共有幾個?
+	public static void lotterySelect (int start, int end) {
+		if (start >= end) {
+			System.out.println("起始值小於等於結束值，請重新輸入");
+		}else {
+			String list = "";
+			int numList = 0 ;
+			for ( ; start <= end; end--) {
+				boolean with4 = false;
+				int testNum = end;
+					for (int i = 2 ; i > 0 ; i--) {
+						if(testNum >= Math.pow(10, i)
+							//判斷是否有該位數（依序由百位往下測試）
+							&& testNum/(int)Math.pow(10, i) == 4){
+							with4 = true;
+							break;
+						}else if (testNum % (int)Math.pow(10, i) == 4){
+							//判斷測試個位數是否為4
+							with4 = true;
+							break;
+						}
+						testNum = testNum % (int)Math.pow(10, i);
+						//去除最大位數，再繼續比較
+					}
+					if (!with4) {
+						list += end+" ";
+						numList++;
+					}
+				}
+			System.out.println("共有"+list+"這些數字可以選，一共有"+numList+"個");
+			}
+		}	
+
+//		• 請設計一隻Java程式,輸出結果為以下:
+//		1 2 3 4 5 6 7 8 9 10
+//		1 2 3 4 5 6 7 8 9
+//		1 2 3 4 5 6 7 8
+//		1 2 3 4 5 6 7
+//		1 2 3 4 5 6
+//		1 2 3 4 5
+//		1 2 3 4
+//		1 2 3
+//		1 2
+//		1
+	public static void drawNumTri () {
+		int triNum = 10;
+		for (int i = triNum ; i >= 1 ; i--) {
+			for(int j = 1 ; j <= i ; j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println("");
+		}
+		
+	}
+//		• 請設計一隻Java程式,輸出結果為以下:
+//			A
+//			BB
+//			CCC
+//			DDDD
+//			EEEEE
+//			FFFFFF
+	public static void drawABCTri (char letterSt, char letterEnd) {
+		for (char i = letterSt ; i <= letterEnd ; i++) {
+			for(char j = letterSt ; j <= i ; j++) {
+				System.out.print(i);
+			}
+			System.out.println("");
+		}
+	}
+//		• 請建立一個TestNineNine.java程式，可輸出九九乘法表
+	public static void testNineNine () {
+		int i = 1 , j = 1 ;
+//		• 一：使用for迴圈 + while迴圈
+		System.out.println("==== First Round ====");
+		for ( ; i <= 9 ; i++ ) {
+			while (j <= 9 ) {
+				System.out.printf(i+"*"+j+"="+i*j+" ") ;
+				j++ ;
+			}
+			System.out.println("");
+			j = 1 ;
+		}
+//		• 二：使用for迴圈 + do while迴圈
+		System.out.println("==== Second Round ====");
+		i = 1; 
+		j = 1;
+		for ( ; i <= 9 ; i++ ) {
+			 do{
+				 j++ ;
+				System.out.printf(i+"*"+j+"="+i*j+" ") ;
+			}while (j < 9 );
+			System.out.println("");
+			j = 1 ;
+		}
+//		• 三：使用while迴圈 + do while迴圈		
+		System.out.println("==== Third Round ====");
+		i = 1; 
+		j = 1;
+			while(i <= 9) {
+			 do{
+				 j++ ;
+				System.out.printf(i+"*"+j+"="+i*j+" ") ;
+			}while (j < 9 );
+			System.out.println("");
+			j = 1 ;
+			i ++ ;
+		}
+		
+	}
+}
